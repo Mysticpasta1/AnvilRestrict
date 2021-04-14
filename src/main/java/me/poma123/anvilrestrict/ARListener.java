ffe0d4c1b082f7ef2f85b6ca1fb4e14c3d0b6c3c
Baterka <bateriecz@gmail.com>
2021-04-15 00:57:20 +0200

    Code cleanup and NullPointerException sanitization

--- src/main/java/me/poma123/anvilrestrict/ARListener.java
+++ src/main/java/me/poma123/anvilrestrict/ARListener.java
@@ -1,7 +1,7 @@
 package me.poma123.anvilrestrict;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Objects;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -15,92 +15,96 @@ import org.bukkit.inventory.ItemStack;
 import static me.poma123.anvilrestrict.AnvilRestrict.withColors;
 
 public class ARListener implements Listener {
-	@SuppressWarnings("unused")
-	private AnvilRestrict plugin;
-	public HashMap<String, String> fixDisplayName = new HashMap<String, String>();
+    private final AnvilRestrict plugin;
 
-	public ARListener(AnvilRestrict p) {
-		this.plugin = p;
-	}
+    public ARListener(AnvilRestrict p) {
+        this.plugin = p;
+    }
 
-	@EventHandler
-	public void onInventoryClick(InventoryClickEvent event) {
-	//debug	event.getWhoClicked().sendMessage(String.valueOf(event.getSlot()));
-		if (event.getSlot() != 64537) {
-			Player p = (Player) event.getWhoClicked();
-			Boolean done = Boolean.valueOf(false);
-			ItemStack item = event.getCurrentItem();
-			if (event.getInventory().getType() == InventoryType.ANVIL) {
-				if (AnvilRestrict.noEntryAll.booleanValue()) {
-					event.setCancelled(true);
-					p.sendMessage(withColors(plugin.getConfig().getString("messages.all_entry_disabled"), '&'));
-				} else if (AnvilRestrict.noRenameAll.booleanValue()) {
-					if (item != null) {
-						String tempname = "";
-						if (item.getItemMeta().hasDisplayName()) {
-							tempname = item.getItemMeta().getDisplayName();
-						}
-						if (!tempname.equals(event.getInventory().getItem(0).getItemMeta().getDisplayName())) {
-							event.setCancelled(true);
-							p.sendMessage(withColors(plugin.getConfig().getString("messages.all_rename_disabled"), '&'));
-						}
-					}
-				} else {
-					for (ArrayList<String> noEntryData : AnvilRestrict.noEntry) {
-						if (!done.booleanValue()) {
-							if ((noEntryData.size() == 2) || (!noEntryData.isEmpty())) {
-								if (((String) noEntryData.get(0)).equalsIgnoreCase("id")) {
-									if ((item != null)
-											&& (item.getType() == Material.getMaterial((String) noEntryData.get(1)))) {
-										done = Boolean.valueOf(true);
-										event.setCancelled(true);
-										p.sendMessage(withColors(plugin.getConfig().getString("messages.noentry"), '&'));
-									}
-								} else if ((((String) noEntryData.get(0)).equalsIgnoreCase("name"))
-										&& (item.getItemMeta() != null) && (item.getItemMeta().hasDisplayName())) {
-									if (item.getItemMeta().getDisplayName().toLowerCase()
-											.indexOf(((String) noEntryData.get(1)).toLowerCase()) >= 0) {
-										done = Boolean.valueOf(true);
-										event.setCancelled(true);
-										p.sendMessage(withColors(plugin.getConfig().getString("messages.noentry"), '&'));
-									}
-								}
-							}
-						}
-					}
-					if (event.getSlot() == 2 && event.getClickedInventory().getType().equals(InventoryType.ANVIL)) {
-						for (ArrayList<String> noRenameData : AnvilRestrict.noRename) {
-							if (!done.booleanValue()) {
-								if ((noRenameData.size() == 2) || (!noRenameData.isEmpty())) {
-									if (((String) noRenameData.get(0)).equalsIgnoreCase("id")) {
-										if ((item != null) && (item.getType() == Material
-												.getMaterial((String) noRenameData.get(1)))) {
-											String tempname = "";
-											if (item.getItemMeta().hasDisplayName()) {
-												tempname = item.getItemMeta().getDisplayName();
-											}
-											if (!tempname.equals(
-													event.getInventory().getItem(0).getItemMeta().getDisplayName())) {
-												event.setCancelled(true);
-												p.sendMessage(ChatColor.RED + "Renaming that item is disabled.");
-												p.sendMessage(withColors(plugin.getConfig().getString("messages.norename"), '&'));
-											}
-										}
-									} else if ((((String) noRenameData.get(0)).equalsIgnoreCase("name"))
-											&& (item.getItemMeta() != null)
-											&& (event.getInventory().getItem(0).getItemMeta().hasDisplayName())) {
-										if (event.getInventory().getItem(0).getItemMeta().getDisplayName().toLowerCase()
-												.indexOf(((String) noRenameData.get(1)).toLowerCase()) >= 0) {
-											event.setCancelled(true);
-											p.sendMessage(withColors(plugin.getConfig().getString("messages.norename"), '&'));
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        //debug	event.getWhoClicked().sendMessage(String.valueOf(event.getSlot()));
+        try {
+            if (event.getSlot() != 64537) {
+                Player p = (Player) event.getWhoClicked();
+                boolean done = Boolean.FALSE;
+                ItemStack item = event.getCurrentItem();
+                if (event.getInventory().getType() == InventoryType.ANVIL) {
+                    if (AnvilRestrict.noEntryAll) {
+                        event.setCancelled(true);
+                        p.sendMessage(withColors(plugin.getConfig().getString("messages.all_entry_disabled"), '&'));
+                    } else if (AnvilRestrict.noRenameAll) {
+                        if (item != null) {
+                            String tempname = "";
+                            if (Objects.requireNonNull(item.getItemMeta()).hasDisplayName()) {
+                                tempname = item.getItemMeta().getDisplayName();
+                            }
+                            if (!tempname.equals(Objects.requireNonNull(Objects.requireNonNull(event.getInventory().getItem(0)).getItemMeta()).getDisplayName())) {
+                                event.setCancelled(true);
+                                p.sendMessage(withColors(plugin.getConfig().getString("messages.all_rename_disabled"), '&'));
+                            }
+                        }
+                    } else {
+                        for (ArrayList<String> noEntryData : AnvilRestrict.noEntry) {
+                            if (!done) {
+                                if (!noEntryData.isEmpty()) {
+                                    if (noEntryData.get(0).equalsIgnoreCase("id")) {
+                                        if ((item != null)
+                                                && (item.getType() == Material.getMaterial(noEntryData.get(1)))) {
+                                            done = Boolean.TRUE;
+                                            event.setCancelled(true);
+                                            p.sendMessage(withColors(plugin.getConfig().getString("messages.noentry"), '&'));
+                                        }
+                                    } else {
+                                        if ((noEntryData.get(0).equalsIgnoreCase("name"))) {
+                                            assert item != null;
+                                            if ((item.getItemMeta() != null) && (item.getItemMeta().hasDisplayName())) {
+                                                if (item.getItemMeta().getDisplayName().toLowerCase().contains(noEntryData.get(1).toLowerCase())) {
+                                                    done = Boolean.TRUE;
+                                                    event.setCancelled(true);
+                                                    p.sendMessage(withColors(plugin.getConfig().getString("messages.noentry"), '&'));
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (event.getSlot() == 2 && Objects.requireNonNull(event.getClickedInventory()).getType().equals(InventoryType.ANVIL)) {
+                            for (ArrayList<String> noRenameData : AnvilRestrict.noRename) {
+                                if (!done) {
+                                    if (!noRenameData.isEmpty()) {
+                                        if (noRenameData.get(0).equalsIgnoreCase("id")) {
+                                            if ((item != null) && (item.getType() == Material
+                                                    .getMaterial(noRenameData.get(1)))) {
+                                                String tempname = "";
+                                                if (Objects.requireNonNull(item.getItemMeta()).hasDisplayName()) {
+                                                    tempname = item.getItemMeta().getDisplayName();
+                                                }
+                                                if (!tempname.equals(
+                                                        Objects.requireNonNull(Objects.requireNonNull(event.getInventory().getItem(0)).getItemMeta()).getDisplayName())) {
+                                                    event.setCancelled(true);
+                                                    p.sendMessage(ChatColor.RED + "Renaming that item is disabled.");
+                                                    p.sendMessage(withColors(plugin.getConfig().getString("messages.norename"), '&'));
+                                                }
+                                            }
+                                        } else if ((noRenameData.get(0).equalsIgnoreCase("name"))
+                                                && (Objects.requireNonNull(item).getItemMeta() != null)
+                                                && (Objects.requireNonNull(Objects.requireNonNull(event.getInventory().getItem(0)).getItemMeta()).hasDisplayName())) {
+                                            if (Objects.requireNonNull(Objects.requireNonNull(event.getInventory().getItem(0)).getItemMeta()).getDisplayName().toLowerCase().contains(noRenameData.get(1).toLowerCase())) {
+                                                event.setCancelled(true);
+                                                p.sendMessage(withColors(plugin.getConfig().getString("messages.norename"), '&'));
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (NullPointerException ignored) {
+
+        }
+    }
 }
--- src/main/java/me/poma123/anvilrestrict/AnvilRestrict.java
+++ src/main/java/me/poma123/anvilrestrict/AnvilRestrict.java
@@ -2,103 +2,119 @@ package me.poma123.anvilrestrict;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Objects;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AnvilRestrict extends JavaPlugin {
-	public static Boolean noEntryAll = Boolean.valueOf(false);
-	public static Boolean noRenameAll = Boolean.valueOf(false);
-	public static ArrayList<ArrayList<String>> noEntry = new ArrayList<ArrayList<String>>();
-	public static ArrayList<ArrayList<String>> noRename = new ArrayList<ArrayList<String>>();
+    public static Boolean noEntryAll = Boolean.FALSE;
+    public static Boolean noRenameAll = Boolean.FALSE;
+    public static ArrayList<ArrayList<String>> noEntry = new ArrayList<>();
+    public static ArrayList<ArrayList<String>> noRename = new ArrayList<>();
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("anvilrestrict")) {
-			if (sender.hasPermission("anvilrestrict.admin")) {
-				if (args.length == 0) {
-					sender.sendMessage("§8[§7AnvilRestrict§8] §cAnvilRestrict HELP:");
-					sender.sendMessage("§8[§7AnvilRestrict§8]");
-					sender.sendMessage("§8[§7AnvilRestrict§8] §6/anvilrestrict reload §f- Reload config");
-					sender.sendMessage("§8[§7AnvilRestrict§8] §cAliases: §7[ar, anvilr]");
-				} else {
-					if (args[0].equalsIgnoreCase("reload")) {
-						if (!new File(getDataFolder(), "config.yml").exists()) {
-							saveDefaultConfig();
-						}
-						if (getConfig().getConfigurationSection("messages") == null) {
-							getConfig().set("messages.noentry", "&cEntering that item in the Anvil is disabled.");
-							getConfig().set("messages.norename", "&cRenaming that item is disabled.");
-							getConfig().set("messages.all_entry_disabled", "&cThe Anvil is disabled.");
-							getConfig().set("messages.all_rename_disabled", "&cRenaming items is disabled.");
-							saveConfig();
-						}
-						reloadConfig();
-						setup();
-						sender.sendMessage("§8[§7AnvilRestrict§8] §fConfig reloaded!");
-					} else {
-						sender.sendMessage("§8[§7AnvilRestrict§8] §cAnvilRestrict HELP:");
-						sender.sendMessage("§8[§7AnvilRestrict§8]");
-						sender.sendMessage("§8[§7AnvilRestrict§8] §6/anvilrestrict reload §f- Reload config");
-						sender.sendMessage("§8[§7AnvilRestrict§8] §cAliases: §7[ar, anvilr]");
-					}
-				}
-			} else {
-				sender.sendMessage("§cYou don't have permission to perform this command!");
-			}
-		}
-		return true;
-	}
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("anvilrestrict")) {
+            if (sender.hasPermission("anvilrestrict.admin")) {
+                if (args.length == 0) {
+                    sender.sendMessage("§8[§7AnvilRestrict§8] §cAnvilRestrict HELP:");
+                    sender.sendMessage("§8[§7AnvilRestrict§8]");
+                    sender.sendMessage("§8[§7AnvilRestrict§8] §6/anvilrestrict reload §f- Reload config");
+                    sender.sendMessage("§8[§7AnvilRestrict§8] §cAliases: §7[ar, anvilr]");
+                } else {
+                    if (args[0].equalsIgnoreCase("reload")) {
+                        saveDefaultConfigIfNotExists();
+                        if (getConfig().getConfigurationSection("messages") == null) {
+                            getConfig().set("messages.noentry", "&cEntering that item in the Anvil is disabled.");
+                            getConfig().set("messages.norename", "&cRenaming that item is disabled.");
+                            getConfig().set("messages.all_entry_disabled", "&cThe Anvil is disabled.");
+                            getConfig().set("messages.all_rename_disabled", "&cRenaming items is disabled.");
+                            saveConfig();
+                        }
+                        reloadConfig();
+                        setup();
+                        sender.sendMessage("§8[§7AnvilRestrict§8] §fConfig reloaded!");
+                    } else {
+                        sender.sendMessage("§8[§7AnvilRestrict§8] §cAnvilRestrict HELP:");
+                        sender.sendMessage("§8[§7AnvilRestrict§8]");
+                        sender.sendMessage("§8[§7AnvilRestrict§8] §6/anvilrestrict reload §f- Reload config");
+                        sender.sendMessage("§8[§7AnvilRestrict§8] §cAliases: §7[ar, anvilr]");
+                    }
+                }
+            } else {
+                sender.sendMessage("§cYou don't have permission to perform this command!");
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public void onEnable() {
-		getServer().getPluginManager().registerEvents(new ARListener(this), this);
-		if (!new File(getDataFolder(), "config.yml").exists()) {
-			saveDefaultConfig();
-		}
-		if (getConfig().getConfigurationSection("messages") == null) {
-			getConfig().set("messages.noentry", "&cEntering that item in the Anvil is disabled.");
-			getConfig().set("messages.norename", "&cRenaming that item is disabled.");
-			getConfig().set("messages.all_entry_disabled", "&cThe Anvil is disabled.");
-			getConfig().set("messages.all_rename_disabled", "&cRenaming items is disabled.");
-			saveConfig();
-		}
-		setup();
-	}
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new ARListener(this), this);
+        saveDefaultConfigIfNotExists();
+        if (getConfig().getConfigurationSection("messages") == null) {
+            getConfig().set("messages.noentry", "&cEntering that item in the Anvil is disabled.");
+            getConfig().set("messages.norename", "&cRenaming that item is disabled.");
+            getConfig().set("messages.all_entry_disabled", "&cThe Anvil is disabled.");
+            getConfig().set("messages.all_rename_disabled", "&cRenaming items is disabled.");
+            saveConfig();
+        }
+        setup();
+    }
 
-	public static String withColors(String input, char ch) {
-		return ChatColor.translateAlternateColorCodes(ch, input);
-	}
+    public static String withColors(String input, char ch) {
+        return ChatColor.translateAlternateColorCodes(ch, input);
+    }
 
-	public void setup() {
-		noEntryAll = Boolean.valueOf(getConfig().getBoolean("NoEntryAll"));
-		noRenameAll = Boolean.valueOf(getConfig().getBoolean("NoRenameAll"));
-		String[] NoEntryTemp = getConfig().getString("NoEntry").split("\\,");
-		String[] arrayOfString1;
-		int j = (arrayOfString1 = NoEntryTemp).length;
-		String[] ettemp;
-		for (int i = 0; i < j; i++) {
-			String EntryTemp = arrayOfString1[i];
-			ettemp = EntryTemp.split("\\|");
-			ArrayList<String> noEntryt = new ArrayList<String>();
-			if (ettemp.length >= 1) {
-				noEntryt.add(ettemp[0]);
-				noEntryt.add(ettemp[1]);
-			}
-			noEntry.add(noEntryt);
-		}
-		String[] NoRenameTemp = getConfig().getString("NoRename").split("\\,");
-		int k = (ettemp = NoRenameTemp).length;
-		for (j = 0; j < k; j++) {
-			String NoRename = ettemp[j];
-			ettemp = NoRename.split("\\|");
-			ArrayList<String> noRenamet = new ArrayList<String>();
-			if (ettemp.length >= 1) {
-				noRenamet.add(ettemp[0]);
-				noRenamet.add(ettemp[1]);
-			}
-			noRename.add(noRenamet);
-		}
-	}
+    public void setup() {
+        try {
+            noEntryAll = getConfig().getBoolean("NoEntryAll");
+            noRenameAll = getConfig().getBoolean("NoRenameAll");
+            int j;
+            try {
+                String[] noEntryArray = Objects.requireNonNull(getConfig().getString("NoEntry")).split(",");
+                j = noEntryArray.length;
+                for (int i = 0; i < j; i++) {
+                    String noEntryTmp = noEntryArray[i];
+                    String[] noEntrySplit = noEntryTmp.split("\\|");
+                    ArrayList<String> noEntryList = new ArrayList<>();
+                    if (noEntrySplit.length == 2) {
+                        noEntryList.add(noEntrySplit[0]);
+                        noEntryList.add(noEntrySplit[1]);
+                    }
+                    noEntry.add(noEntryList);
+                }
+            } catch (NullPointerException e) {
+                getLogger().warning("NoEntry config field is empty or not set at all...");
+            }
+            try {
+                String[] noRenameArray = Objects.requireNonNull(getConfig().getString("NoRename")).split(",");
+                j = noRenameArray.length;
+                for (int i = 0; i < j; i++) {
+                    String noRenameTmp = noRenameArray[i];
+                    String[] noRenameSplit = noRenameTmp.split("\\|");
+                    ArrayList<String> noRenameList = new ArrayList<>();
+                    if (noRenameSplit.length == 2) {
+                        noRenameList.add(noRenameSplit[0]);
+                        noRenameList.add(noRenameSplit[1]);
+                    }
+                    noRename.add(noRenameList);
+                }
+            } catch (NullPointerException e) {
+                getLogger().warning("NoRename config field is empty or not set at all...");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+    }
+
+    private void saveDefaultConfigIfNotExists() {
+        if (!new File(getDataFolder(), "config.yml").exists()) {
+            saveDefaultConfig();
+        }
+    }
 }
